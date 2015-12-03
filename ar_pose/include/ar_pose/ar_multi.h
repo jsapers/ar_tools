b0297a68a03d876f4c1d2ed2bc155de8b548f21f
Liani Lye <lye.liani@gmail.com>
2015-12-02 20:14:37 -0500

    changed camera topic path from absolute to relative

--- ar_pose/include/ar_pose/ar_multi.h
+++ ar_pose/include/ar_pose/ar_multi.h
@@ -63,8 +63,8 @@
 #include <ar_pose/ARMarker.h>
 #include <ar_pose/object.h>
 
-const std::string cameraImageTopic_ = "/camera/image_raw";
-const std::string cameraInfoTopic_  = "/camera/camera_info";
+const std::string cameraImageTopic_ = "camera/image_raw";
+const std::string cameraInfoTopic_  = "camera/camera_info";
 
 const double AR_TO_ROS = 0.001;
 
